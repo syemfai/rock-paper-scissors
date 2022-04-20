ec5dd5eaffd5c8a43af24b75fda75a5a84e3177e
Syed Faizal <syemfai@gmail.com>
2022-04-20 20:55:57 +0800

    Track score of game

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
-function play(playerSelection, computerSelection) {
-    
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "The game is a tie"
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            globalThis.computerScore++ ;
+            return "You Lose! Paper beats Rock";
+        }
+        else {
+            globalThis.playerScore++ ;
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            globalThis.computerScore++ ;
+            return "You Lose! Scissors beats Paper";
+        }
+        else {
+            globalThis.playerScore++ ;
+            return "You Win! Paper beats Rock";
+        }
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            globalThis.playerScore++ ;
+            return "You Win! Scissors beats Paper";
+        }
+        else {
+            globalThis.computerScore++ ;
+            return "You Lose! Rock beats Scissors";
+        }
+    }
 }
 
-console.log(computerPlay());
+function game() {
+    const playerSelection = "rock";
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(globalThis.computerScore);
+    }
+}
+
+
+game()
